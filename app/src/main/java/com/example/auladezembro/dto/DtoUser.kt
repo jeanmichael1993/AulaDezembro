09935c9992cab88c91ea7a2f2d63abeab3aff504
jeanmichael1993 <jeanmichael760@gmail.com>
2021-01-02 17:02:01 -0300

    Adicionado Cadastro Usuario

---
+++ app/src/main/java/com/example/auladezembro/dto/DtoUser.kt
@@ -0,0 +1,9 @@
+package com.example.auladezembro.dto
+
+class DtoUser {
+    var email: String? = null
+    var id = 0
+    var name: String? = null
+    var password: String? = null
+    var phone: String? = null
+}
